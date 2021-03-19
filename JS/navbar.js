7524db7cef2bb0c0492930f2465cd1c4cd132c90
IreneuszSob <ireksobol10@gmail.com>
2021-03-19 11:29:38 +0100

    JS

---
+++ JS/navbar.js
@@ -0,0 +1,5 @@
+let nav = '<nav><a href="index.html">Home<a/>' + '&nbsp;' +
+ '<a href="kontakt.html">Kontakt</a></nav>';
+
+ let element = document.querySelector(".navbar");
+ element.innerHTML=nav;
